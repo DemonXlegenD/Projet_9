26c2c13c7d4ab8494cd9a99a54150c605c5139a8
Cookie <yumzuko@gmail.com>
2024-01-29 12:39:13 +0100

    All ai folder with each AI

--- Projet_9/AI/AITrainer.cs
+++ Projet_9/AI/AITrainer.cs
@@ -3,17 +3,12 @@ namespace Csharp_Tpt
 	public enum AITrainerParameter{
 	   Child, // Random 
 	   Newbie, // Best Move, doesnt care if it's an attack or not
-	   Jarod,
-	   Magnus,
-	   Hacker, // Knows all 
-	   None
+	   Jarod, // Best Move, care if it's an attack, if low heal if he can
+	   Magnus, // Utilise toutes les données que l'on peut normalement avoir et regarde
+	   Hacker, // Knows all , does the best thing regarding all the things he knows
+	   None // ?????
 	}
 
-    public enum States{
-        LowHealth,
-        HightHealth,
-        Other
-    }
     public class AITrainer{
         States STATE = States.Other;
     }
