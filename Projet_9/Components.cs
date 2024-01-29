037954f286ed1af5993eaa9818439ccdeaf4136b
DemonXlegenD <flahalle@gaming.tech>
2024-01-29 13:04:13 +0100

    NEW: Création du projet
    - Réalisation du projet

---
+++ Projet_9/Components.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NComponents
+{
+    internal abstract class Components
+    {
+        private string _name = "components";
+
+        public abstract void Update();
+
+        public abstract void Render();
+
+        public string Name { get { return _name; } set { _name = value; } }
+    }
+}
