4d9d9a0b625f75e1f99917b519a21fec27270aea
DemonXlegenD <flahalle@gaming.tech>
2024-02-01 11:24:59 +0100

    NEW: Création d'un User
    - Création d'un User
    - Sauvegarde d'un User

--- Projet_9/Program.cs
+++ Projet_9/Program.cs
@@ -4,6 +4,9 @@ using NEntity;
 using NPotionType;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Projet_9.Save;
+using NSecurity;
+using System;
 namespace Projet_9
 {
     internal class Program
@@ -16,19 +19,25 @@ namespace Projet_9
 
              engine.Run();
              window.WindowClose();*/
-            Player player = new Player();
-            List<string> list = new List<string>();
-            list.Add("Water");
-            List<JsonConverter> listConverter = new List<JsonConverter>();
-            listConverter.Add(new PlayerJsonConverter());
-            listConverter.Add(new PokemonJsonConverter());
+            /*  Player player = new Player();
+              List<string> list = new List<string>();
+              list.Add("Water");
+              List<JsonConverter> listConverter = new List<JsonConverter>();
+              listConverter.Add(new PlayerJsonConverter());
+              listConverter.Add(new PokemonJsonConverter());
 
-            player.AddItem(new Potion());
-            player.AddPokemon(new Pokemon("Jarod", list, 40, 50, 70, 50, 50, 70));
+              player.AddItem(new Potion());
+              player.AddPokemon(new Pokemon("Jarod", list, 40, 50, 70, 50, 50, 70));
 
-            Save.GetInstance().WriteSave(player, 2, listConverter);
-  /*          Player player = Save.GetInstance().ReadTestSave<Player>(2);
-            Console.WriteLine(player.ToString());*/
+              SavePlayer.GetInstance(player.FirstName, player.LastName, player.Id).WriteSave(player, 2, listConverter);*/
+            /*          Player player = Save.GetInstance().ReadTestSave<Player>(2);
+                      Console.WriteLine(player.ToString());*/
+/*            SaveUser saveUser = SaveUser.GetInstance();*/
+            UserManager userManager = UserManager.GetInstance();
+     /*       userManager.AddUser("Zarod", "azerty");
+            saveUser.SaveUsers();*/
+            User.LoadUser("Zarod", "azerty");
+            Console.WriteLine("C'est bien : " + userManager.ActualUser.Username);
         }
     }
     public class Person
---
+++ Projet_9/Save/SavePlayer.cs
@@ -0,0 +1,129 @@
+
+
+using NEntity;
+using Newtonsoft.Json;
+using NJSON;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NSave
+{
+    public class SavePlayer
+    {
+        static SavePlayer instance;
+
+        private JsonDevelopper jsonSaver;
+        private int _actualIndex = 1;
+        private string _fileName = "Save";
+        private string _folderName = "Saves";
+        private string _fileType = ".json";
+        private string _playerTag = string.Empty;
+
+        public string Name
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        public SavePlayer()
+        {
+            jsonSaver = JsonDevelopper.GetInstance();
+            jsonSaver.CreateFolder(_folderName);
+        }
+
+        public SavePlayer(string playerFirstname, string playerLastname, string playerUid)
+        {
+            jsonSaver = JsonDevelopper.GetInstance();
+            jsonSaver.CreateFolder(_folderName);
+            _playerTag = playerFirstname + "_" + playerLastname + "_" + playerUid;
+            jsonSaver.CreateFolder(_folderName +"/" + _playerTag);
+        }
+
+        public static SavePlayer GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SavePlayer();
+            }
+            return instance;
+        }
+
+        public static SavePlayer GetInstance(string playerFirstname, string playerLastname, string playerUid)
+        {
+            if (instance == null)
+            {
+                instance = new SavePlayer(playerFirstname, playerLastname, playerUid);
+            }
+            return instance;
+        }
+
+        public void ApplySave(object data)
+        {
+            jsonSaver.CreateFile(data, _folderName + "/" + _fileName + _actualIndex + _fileType);
+        }
+        public void ApplySave(object data, int indexSave)
+        {
+            if (_actualIndex != indexSave)
+            {
+                _actualIndex = ((indexSave - 1) % 3) + 1;
+            }
+            jsonSaver.CreateFile(data, _folderName + "/" + _fileName + _actualIndex + _fileType);
+        }
+
+        public object ReadSave()
+        {
+            object objet = jsonSaver.DeserializeJsonFromFile<object>(_folderName + "/" + _fileName + _actualIndex + _fileType);
+            return objet;
+        }
+
+        public object ReadSave(int indexSave)
+        {
+            if (_actualIndex != indexSave)
+            {
+                _actualIndex = ((indexSave - 1) % 3) + 1;
+            }
+            object objet = jsonSaver.DeserializeJsonFromFile<object>(_folderName + "/" + _fileName + _actualIndex + _fileType);
+            return objet;
+        }
+        public T ReadSave<T>()
+        {
+            T objet = jsonSaver.DeserializeJsonFromFile<T>(_folderName + "/" + _fileName + _actualIndex + _fileType);
+            return objet;
+        }
+
+
+        public T ReadSave<T>(int indexSave)
+        {
+            if (_actualIndex != indexSave)
+            {
+                _actualIndex = ((indexSave - 1) % 3) + 1;
+            }
+            T objet = jsonSaver.DeserializeJsonFromFile<T>(_folderName + "/" + _fileName + _actualIndex + _fileType);
+            return objet;
+        }
+        public T ReadTestSave<T>(int indexSave)
+        {
+            if (_actualIndex != indexSave)
+            {
+                _actualIndex = ((indexSave - 1) % 3) + 1;
+            }
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new PlayerJsonConverter());
+
+            using (StreamReader sr = new StreamReader(_folderName + "/" + _fileName + _actualIndex + _fileType))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                return serializer.Deserialize<T>(reader);
+            }
+        }
+
+        public void WriteSave(object data, int indexSave, List<JsonConverter> converters)
+        {
+            if (_actualIndex != indexSave)
+            {
+                _actualIndex = ((indexSave - 1) % 3) + 1;
+            }
+            jsonSaver.SerializeObjectToJsonFile(data, _folderName + "/" + _playerTag + "/" + _fileName +  _actualIndex + _fileType, converters);
+        }
+    }
+}
