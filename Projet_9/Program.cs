94d72de4542cab2738fcbf86542277e53257835a
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-02-01 12:59:41 +0100

    window essaye, fight print

--- Projet_9/Program.cs
+++ Projet_9/Program.cs
@@ -11,11 +11,67 @@ using NScene;
 using Projet_9.Save;
 using NSecurity;
 using System;
+using System.Runtime.InteropServices;
 
 namespace Projet_9
 {
     internal class Program
     {
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleScreenBufferInfo(IntPtr hConsoleOutput, out CONSOLE_SCREEN_BUFFER_INFO lpConsoleScreenBufferInfo);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COORD
+        {
+            public short X;
+            public short Y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SMALL_RECT
+        {
+            public short Left;
+            public short Top;
+            public short Right;
+            public short Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CONSOLE_SCREEN_BUFFER_INFO
+        {
+            public COORD dwSize;
+            public COORD dwCursorPosition;
+            public ushort wAttributes;
+            public SMALL_RECT srWindow;
+            public COORD dwMaximumWindowSize;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleWindowInfo(IntPtr hConsoleOutput, bool bAbsolute, [In] ref SMALL_RECT lpConsoleWindow);
+
+        static void SetConsoleWindowSize(int width, int height)
+        {
+            IntPtr consoleOutput = GetStdHandle(-11); // STD_OUTPUT_HANDLE
+            CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+            GetConsoleScreenBufferInfo(consoleOutput, out bufferInfo);
+
+            SMALL_RECT rect = new SMALL_RECT
+            {
+                Left = 0,
+                Top = 0,
+                Right = (short)(width - 1),
+                Bottom = (short)(height - 1)
+            };
+
+            SetConsoleWindowInfo(consoleOutput, true, ref rect);
+        }
+
+
         static void Main(string[] args)
         {
 /*            List<Pokemon> Pokemons = new List<Pokemon>();
@@ -49,6 +105,9 @@ namespace Projet_9
             /*WindowPokemonTeam window = new WindowPokemonTeam();
             window.WindowRun();*/
             Engine engine = Engine.GetInstance();
+
+            SetConsoleWindowSize(50, 50);
+
 
 
             engine.Run();
--- Projet_9/Scene/FightScene.cs
+++ Projet_9/Scene/FightScene.cs
@@ -1,10 +1,13 @@
-﻿using NModules;
+﻿using NGlobal;
+using NModules;
+using NPokemon;
 using NScene;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace NScene
 {
@@ -17,17 +20,33 @@ namespace NScene
             CHANGE,
             NOTHING
         }
+
         private States STATE = States.SELECT;
 
+        List<Pokemon> List1;
+        List<Pokemon> List2;
+
+        Pokemon P1;
+        Pokemon P2;
 
         public FightScene() : base("FightScene")
         {
+
+            List1 = new List<Pokemon>() { new Pokemon("jarod", new List<string> { "Water" }, 10, 10, 10, 10, 10, 10, 5) };
+            List1[0].Moves.Add(new Attack("Charge", "Normal", "Physical", 20, 100, 25));
+            List2 = new List<Pokemon>() { new Pokemon("maurad", new List<string> { "Grass" }, 10, 10, 10, 10, 10, 10, 5) };
+            List2[0].Moves.Add(new Attack("Charge", "Normal", "Physical", 20, 100, 25));
+
+            P1 = List1[0];
+            P2 = List2[0];
 
         }
 
         public override void Update(float deltaTime)
         {
             base.Update(deltaTime);
+            ToWrite();
+            Console.ReadKey();
             Console.WriteLine("Update : Hello it's new here");
         }
 
@@ -36,14 +55,45 @@ namespace NScene
         {
             // Write le perso et le perso ennemie
 
+            PokemonInfo(P2);
+            SauterLignes(2);
+            PokemonInfo(P1);
+
+
             // En fonction du state, print les choix dispo
 
 
         }
 
+        private void SauterLignes(int x)
+        {
+            for (int i = 0; i < x; i++) { Console.WriteLine(" "); }
+        }
+
+        private void PokemonInfo(Pokemon P)
+        {
+            if (P.Types.Count > 1) 
+            {
+                Console.Write(P.Name + " ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(P.Hp + "/" + P.MaxHp + " ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(P.Types[0]+" ");
+                Console.Write(P.Types[1]);
+            }
+            else 
+            { 
+                Console.Write(P.Name + " ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(P.Hp+"/"+P.MaxHp+" ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(P.Types[0]);
+            }
+        }
+
         public override void Render()
         {
-            Console.WriteLine("Render : Hello it's new here");
+            //Console.WriteLine("Render : Hello it's new here");
         }
     }
 }
